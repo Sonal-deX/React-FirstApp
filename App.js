a7f7490ed2d29574c27ce92db90f3703271babc6
Sonal Attanayake <sonalattanayake@Sonals-MacBook-Pro.local>
2023-06-11 09:37:02 +0530

    make screens

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1} onPress={() => { console.log("yguygy") }} >Open up App.js to start working on!</Text>
+     
+      <Button  title="Submit"/>
     </View>
   );
 }
